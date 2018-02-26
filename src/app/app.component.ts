ed0cc94c6f62e1484660194ece89e0b94e584e48
dhananjaybelhekar <dhananjaybelhekar@gmail.com>
2018-02-26 16:53:06 +0530

    ng store

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
 import { Component } from '@angular/core';
 
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs/Observable';
+
+interface AppState {
+  message: string;
+}
+
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
-  title = 'app';
+
+  message$: Observable<string>
+
+  constructor(private store: Store<any>) {
+    this.message$ = this.store.select('message')
+  }
+
+  spanishMessage() {
+    this.store.dispatch({type: 'SPANISH'})
+  }
+
+  frenchMessage() {
+    this.store.dispatch({type: 'FRENCH'})
+  }
 }
